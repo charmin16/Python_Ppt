7803de52ab0713fbb7cd502f97f199690d6adfd3
Charming Princewill <prinzecharmin101@gmail.com>
2021-09-13 17:00:29 +0100

    zion1

---
+++ basics_2.py
@@ -0,0 +1,48 @@
+#strings
+#course = "Python's for Beginners"
+#course_1 = 'Python for "Beginners"'
+#print(course)
+#print(course_1)
+#course_2 = 'Python for Beginners'
+#index      0123456789...
+#                      ...-3-2-1
+#print(course_1[-3])
+#print(course[1:]) #python will assume index 1(in this case, y) as the start index
+#print(course[:5]) #python will assume index 5(in this case, n) as the last 
+#print(course[2:-2])
+
+#Formatted Strings
+#first = 'John'
+#last  = 'Smith'
+#message = first +' [' + last + '] is a coder'
+#print(message)
+#msg = f'{first} [{last}] is a coder'
+#print(msg)
+
+#first_name = 'Charming'
+#last_name  = 'Princewill'
+#sentence = f'My name is {first_name} {last_name}'
+#print(sentence)
+
+#String methods
+course = 'Python for Beginners'
+len(course) # This is to calculate the number of characters in a variable, its used
+#also to caluclate the number of items in a list and etc
+print(len(course))
+print(course.upper()) # For converting all the characters to upper case
+print(course.lower()) # For converting all the characters to lower case
+print(course)
+
+course.find('P')
+print(course.find('P')) # This function will return 0 because the character 'P'in in index 0.
+# You can use same function to find out the indices of each of the characters in a string
+print(course.find('Beginners')) # This will return 11 because the word begins with B and B
+# is index 11
+course.replace('Beginners', 'Absolute Beginners')
+print(course.replace('Beginners', 'Absolute Beginners')) # This function is to replace Beginners
+# with Absolute Beginners. You can do same with single characters
+print(course.replace('P', 'T')) # function to replace P with T. Its case sensitive
+'Python' in course
+print('Python' in course) # This is a funtion to check if a particular character or sequence
+# of characters is in a string. It returns TRUE or FALSE
+print(f"")
---
+++ basics_3.py
@@ -0,0 +1,30 @@
+# Arithmetic Operations
+#print( 10 + 3 ) # Addition operation
+#print( 10 - 3 ) # Subtraction operation#print( 10 * 3 ) # Multiplication operation
+#print( 10 / 3 ) # Division operation, it will return a floating number: 3.33333..
+#print( 10 // 3) # Division operation that returns an integer: 3
+#print( 10 % 3 ) # Returns the remainder of the division operation which is 1
+#print( 10 ** 3 ) # Double asterics or multiplication sign means 10 raised to power 3: 1000
+
+#x = 10
+#x =  x + 3
+#print(x)
+#Alternatively
+#x = 10
+#x += 3 #Augmented assignment. This is same as whats on line 10. The value of x is x + 3
+#print(x)
+#x -= 3 # The value of x is x -3
+#print(x)
+#x *= 3 # The value of x(meanwhile x is now 7) is x*3
+#print(x)
+#x /=3  # The new value of x is now 21.  This will return x/3
+#print(x)
+
+# Maths operations
+#y = 2.4
+#print(round(y)) # This function is to round up or down to the nearest whole number
+#print(abs(-7.9)) # Returns the postive representation of the number even if its negative
+
+import math
+print(math.ceil(6.4))
+print(math.floor(6.4))
