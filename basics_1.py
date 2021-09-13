0768120f7e22ed8a6d2574af32ff6f12449ee0b7
Charming Princewill <prinzecharmin101@gmail.com>
2021-09-13 16:59:34 +0100

    zion

---
+++ basics_1.py
@@ -0,0 +1,56 @@
+print('o----')
+print (' ||||')
+print('*' * 10)
+
+#Variables : They temporarily store data in a computer's memory
+#price = 10 # price is the variable or identifier if you like, while 10 is the value
+#price = 20
+#print(price)
+
+#price = 10 # integer
+#rating = 4.9 # floating
+#name = 'Charming' # string
+#is_published = False  # Boolean. We use underscore to indicate multiple words in a variable's name
+#print(price)
+
+#Exercise: Write a program for a hospital, we check in a patient's name John Smith. He's 20yrs
+#old and a new patient. Define three variables for his name, his age, and another variable to 
+#tell if he's a new or existing patient
+
+#Patient_name = 'John Smith' #stating the patient's name
+#Patient_age =  20 #stating the patient's age
+#is_new = True #stating the patients status whether new or existing
+
+#Getting inputs
+#my_name = raw_input('What is your name? ')
+#print('Hello ' + my_name )
+
+#Ask two questions: person's name and favourite color, then print a msg like
+#Isabella likes purple.
+
+#name = raw_input('What is your name? ')
+#favourite_color = raw_input('What is your favourtie color? ')
+#print(name + ' likes ' + favourite_color)
+
+#Type Conversion
+#Write a program to write the year we were born in, then it will calculate our age
+#and put it on the terminal
+
+#birth_year = raw_input('Birth year: ')
+#age = 2021 - birth_year #This is definitely goin to give you an error msg because
+# 2021 is an integer and birth_year is a string. For the operation to work, the string has
+# to be converted to integer as shown below
+#age = 2021 - int(birth_year)
+#print(age)
+
+#birth_year = raw_input('Birth year: ')
+#print(type(birth_year))
+#print(type(age))
+
+#Exercise: Ask a user their weight (in pounds), convert it to kilograms and print on the terminal
+
+#user_weight = raw_input('What is your weight in pounds: ')
+#weight_kg = float(user_weight)/2.2
+#print(weight_kg) 
+
+
